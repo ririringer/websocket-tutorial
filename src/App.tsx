4209d9368e27921a59744c9069eb97ff5c0af449
honnmadesu6 <honnmadesu6@gmail.com>
2022-09-05 22:35:54 +0900

    チュートリアルを参考に作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState("");
+  const webSocketRef = useRef<WebSocket>();
+
+  useEffect(() => {
+    const socket = new WebSocket("wss://scefiw81wf.execute-api.ap-northeast-1.amazonaws.com/production");
+    webSocketRef.current = socket;
+
+    socket.addEventListener("message", event => {
+      setMessage(event.data);
+    });
+
+    return () => socket.close();
+  }, []);
+
+  const [inputText, setInputText] = useState("");
+  const submit: React.FormEventHandler = useCallback(
+    event => {
+      event.preventDefault();
+      webSocketRef.current?.send(`{"action": "sendmessage", "message": "${inputText}"}`);
+    },
+    [inputText],
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{JSON.stringify(message)}</h1>
+      <form onSubmit={submit}>
+        <input value={inputText} onChange={e => setInputText(e.target.value)} />
+        <button>送信</button>
+      </form>
     </div>
   );
 }
